a7f3b77803ffe9d98d565369816e1039b4cd6ced
wellington.siqueira <wellingtonex@gmail.com>
2018-11-14 23:23:01 -0200

    criando o primeiro component - photo.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
-  description = 'Ona 01';
-  url = 'https://scontent-sea1-1.cdninstagram.com/vp/bfe08a2392416893aa8685c8eead5929/5C6FCABB/t51.2885-15/e35/43388060_157867981816582_4340901262561116160_n.jpg?ig_cache_key=MTYzNDM0Nzg3NzE0MzU2Njk2Mg%3D%3D.2';
 }
