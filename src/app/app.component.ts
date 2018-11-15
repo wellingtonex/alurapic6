8480655fdeb045a911e13c7812c2acda3252a5b5
wellington.siqueira <wellingtonex@gmail.com>
2018-11-15 13:46:31 -0200

    explorando o ciclo de vida de um component com ngOnInit e movendo a dependencia de HttpClientModule para photos.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
@@ -9,11 +9,15 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   photos: Photo[] = [];
 
-  constructor(fotoService: PhotoService) {
-    fotoService.listFromUser('flavio')
+  constructor(private fotoService: PhotoService) {
+  }
+
+  ngOnInit(): void {
+    this.fotoService.listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { PhotosModule } from './photos/photos.module';
 import { AppComponent } from './app.component';
@@ -11,7 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     PhotosModule
   ],
   providers: [],
