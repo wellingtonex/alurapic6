49f4b50343b08ae5b14383b3a062fc5f01fba2f9
wellington.siqueira <wellingtonex@gmail.com>
2018-11-15 16:58:49 -0200

    criando o component photo-list

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
-    PhotoListComponent
+    PhotoListComponent,
+    PhotoFormComponent
   ],
   exports: [],
   imports: [
