f8d136368ee73143f6b4956afe0e1b814667fd0c
wellington.siqueira <wellingtonex@gmail.com>
2018-11-15 12:54:08 -0200

    importando o modulo HttpClientModule e injetando um HttpClient em app.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PhotosModule } from './photos/photos.module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     PhotosModule
   ],
   providers: [],
