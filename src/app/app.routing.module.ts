7ca65774a4c0970c42c650e3a6117b5775ad02ce
wellington.siqueira <wellingtonex@gmail.com>
2018-11-17 11:55:09 -0200

    implementando um resolver (para carregar as photos antes de resolver uma rota)

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,9 +5,12 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './erros/not-found/not-found.component';
 import { ErrosModule } from './erros/erros.module';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-  {path: 'user/:userName', component: PhotoListComponent},
+  {path: 'user/:userName', component: PhotoListComponent, resolve: {
+    photos: PhotoListResolver
+  }},
   {path: 'photo/add', component: PhotoFormComponent},
   {path: '**', component: NotFoundComponent}
 ];
