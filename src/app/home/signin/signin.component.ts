07350f8bc3bd072730d558acfbdb672140521676
wellington.siqueira <wellingtonex@gmail.com>
2018-11-21 23:05:29 -0200

    acessando um elemento do dom no controller, com a finalidade de setar o foco para o input user name apos um login que resultou em falha

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Subject } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { AuthService } from './../../core/auth.service';
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -38,6 +39,7 @@ export class SigninComponent implements OnInit {
         }, error => {
         console.error(error);
         this.loginForm.reset();
+        this.userNameInput.nativeElement.focus();
       }
     );
 
