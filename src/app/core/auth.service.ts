7ba63e5e1a101d5eef5f39ad91271c58f972f7ed
wellington.siqueira <wellingtonex@gmail.com>
2018-11-21 22:44:57 -0200

    criando serviço e metodo para autentição no controller

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { stringify } from 'querystring';
+
+const API_URL = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+   }
+
+   authenticate(userName: string, password: string) {
+     return this.http.post(API_URL + '/user/login', {userName, password});
+  }
+}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
+import { Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { AuthService } from './../../core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -8,13 +11,31 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.authenticate(userName, password)
+      .subscribe(
+        () => console.log('autenticado')
+      , error => {
+        console.error(error);
+        this.loginForm.reset();
+      }
+    );
+
   }
 
 }
