bf9888a1a00f6995869b11c716013fdb7bfcca88
wellington.siqueira <wellingtonex@gmail.com>
2018-11-27 21:27:09 -0200

    implementando validações do formulario de singup

--- src/app/home/singup/singup.component.ts
+++ src/app/home/singup/singup.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,9 +6,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ],
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]
+      ],
+      password: ['',
+        [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(14)
+        ]
+      ]
+    });
   }
 
 }
