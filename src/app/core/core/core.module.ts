c055a71a33605bbc0943d7525cf7aae7d6ffceea
wellington.siqueira <wellingtonex@gmail.com>
2018-11-29 00:24:37 -0200

    adicionando o token nas requisições atraves de um interceptor

--- src/app/core/core/core.module.ts
+++ src/app/core/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from '../auth/request.interceptor';
 
 @NgModule({
   imports: [
@@ -9,6 +11,13 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   declarations: [ HeaderComponent],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
