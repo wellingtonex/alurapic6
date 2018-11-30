87d4e108710cf85823aeede7c3d8b0fcbfeb7d6a
wellington.siqueira <wellingtonex@gmail.com>
2018-11-29 22:19:40 -0200

    criando um footer

--- src/app/core/core/core.module.ts
+++ src/app/core/core/core.module.ts
@@ -1,3 +1,4 @@
+import { FooterComponent } from './../footer/footer.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
@@ -10,8 +11,14 @@ import { RequestInterceptor } from '../auth/request.interceptor';
     CommonModule,
     RouterModule
   ],
-  declarations: [ HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
