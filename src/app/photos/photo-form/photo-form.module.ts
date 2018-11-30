ba380f70b22873925d27ca95d7e667547eab32a2
wellington.siqueira <wellingtonex@gmail.com>
2018-11-29 22:33:51 -0200

    criando o formulario de photo-form.component.html

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,19 @@
+import { VMessageModule } from './../../shared/components/vmessage/vmessage.module';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    declarations: [
+      PhotoFormComponent
+    ],
+    imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      VMessageModule,
+      FormsModule
+    ]
 })
-export class PhotoFormModule { }
+export class PhotoFormModule {
+ }
