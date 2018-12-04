c86d4bdf3fcaf8a371396ada1333279222402cc9
wellington.siqueira <wellingtonex@gmail.com>
2018-12-04 20:18:38 -0200

    corrigindo pequeno bug

--- src/app/photos/photo-details/photo-owner-only/photo-owner-only.directive.ts
+++ src/app/photos/photo-details/photo-owner-only/photo-owner-only.directive.ts
@@ -18,7 +18,7 @@ export class PhotoOwnerOnlyDirective implements OnInit {
   ngOnInit(): void {
     this.userService.getUser()
       .subscribe(user => {
-        if (!user || (user.id != this.ownedPhoto.userId) {
+        if (!user || (user.id != this.ownedPhoto.userId)) {
           this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
         }
       });
