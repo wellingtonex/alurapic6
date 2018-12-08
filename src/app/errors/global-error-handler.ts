51d833f227f8681bfb662c6f9e2a34da7499eff3
wellington.siqueira <wellingtonex@gmail.com>
2018-12-08 01:28:20 -0200

    copiando global-error-handler do Flavio

--- src/app/errors/global-error-handler.ts
+++ src/app/errors/global-error-handler.ts
@@ -1,46 +1,42 @@
-import { environment } from './../../environments/environment';
-import { UserService } from './../core/user/user.service';
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { Router } from '@angular/router';
 
+import { Router } from '@angular/router';
+import { UserService } from '../core/user/user.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
-  constructor(private injector: Injector) {}
+    constructor(private injector: Injector) {}
 
-  handleError(error: any): void {
+    handleError(error: any): void {
 
-    const location = this.injector.get(LocationStrategy);
-    const userService = this.injector.get(UserService);
-    const router = this.injector.get(Router);
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+        const router = this.injector.get(Router);
 
-    const url = location instanceof PathLocationStrategy
-      ? location.path : '';
+        const url = location instanceof PathLocationStrategy
+            ? location.path()
+            : '';
 
-    console.log('passei pelo handler');
+        const message = error.message
+            ? error.message :
+            error.toString();
 
-    const message = error.message ? error.message : error.toString();
+        router.navigate(['/error']);
 
-    if (environment.production) {
-      router.navigate(['/error']);
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n');
+
+                console.log(message);
+                console.log(stackAsString);
+                console.log('o que será enviado para o servidor');
+            });
+
     }
-
-    StackTrace.fromError(error)
-      .then(stackFrames => {
-        const stackAsString = stackFrames
-          .map(sf => sf.toString)
-          .join('\n');
-          console.log(message);
-          console.log(stackAsString);
-
-          console.log({
-            message,
-            url,
-            userName: userService.getUserName(),
-            stack: stackAsString});
-      });
-  }
 }
